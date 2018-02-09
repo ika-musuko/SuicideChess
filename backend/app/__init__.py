28f327e3ea4035d0b9214c5ece4bdb4c69e13b0e
Sherwyn Sen <sherwyn.sen@gmail.com>
2018-02-09 02:39:57 -0800

    removed the backend folder for now, will re-add once complete

--- backend/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from firebase import firebase
-
-app = Flask(__name__)
-firebase = firebase.FirebaseApplication('https://flaskbase.firebaseio.com', 
-None)
-
-from app import routes, forms

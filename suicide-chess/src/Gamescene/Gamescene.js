c8c033042a08e8174002b01155d15825fb781d61
Eric Scaramuzzo <eric.scaramuzzo@gmail.com>
2018-04-26 16:47:02 -0700

    Online game working

--- suicide-chess/src/Gamescene/Gamescene.js
+++ suicide-chess/src/Gamescene/Gamescene.js
@@ -4,6 +4,7 @@ import './Gamescene.css'
 import { observe } from '../Game'
 import { pieceObserve } from '../Game'
 import { getValidMoves } from '../Utilities/GetValidMoves'
+import { movePiece } from '../Game'
 import firebase from '../firebase'
 
 class gamescene extends Component {
@@ -55,7 +56,10 @@ class gamescene extends Component {
 
       submitted: false,
 
-      user1: '',
+      isWhite: false,
+      username: '',
+      otherUser: '',
+      gameID: '',
 
       selectedPiece: null
     }
@@ -84,12 +88,20 @@ class gamescene extends Component {
 
       if(blackWin || whiteWin) {
         this.setState({
-          whiteWin: whiteWin, 
+          whiteWin: whiteWin,
           blackWin: blackWin,
         });
       }
 
       if(this.state && changesMade) {
+        var gameData = pieces;
+        gameData['whiteTurn'] = changesMade ? !this.state.whiteTurn : this.state.whiteTurn;
+        var postData = {
+          gameData: gameData,
+        }
+        var updates = {};
+        updates['/games/' + this.state.gameID + '/gameData/'] = gameData;
+        firebase.database().ref().update(updates);
         this.setState({
           validTiles: [],
           ...pieces,
@@ -107,42 +119,95 @@ class gamescene extends Component {
 
   handlePieceSelect (piece) {
     if(this.state.submitted) {
-      let validTiles = getValidMoves(this.state, piece)
-      if(piece !== null && this.state.whiteTurn && piece.substring(0,5) === "white") {
-        this.setState({
-          validTiles: validTiles,
-          selectedPiece: piece
-        });
-      } else if (piece !== null && !this.state.whiteTurn && piece.substring(0,5) === "black") {
-        this.setState({
-          validTiles: validTiles,
-          selectedPiece: piece
-        });
+      if((this.state.isWhite && this.state.whiteTurn) || (!this.state.isWhite && !this.state.whiteTurn)) {
+        let validTiles = getValidMoves(this.state, piece)
+        if(piece !== null && this.state.isWhite && this.state.whiteTurn && piece.substring(0,5) === "white") {
+          this.setState({
+            validTiles: validTiles,
+            selectedPiece: piece
+          });
+        } else if (piece !== null && !this.state.isWhite && !this.state.whiteTurn && piece.substring(0,5) === "black") {
+          this.setState({
+            validTiles: validTiles,
+            selectedPiece: piece
+          });
+        }
       }
     }
   };
 
   handleFormChange (event) {
-    this.setState({user1: event.target.value});
+    this.setState({username: event.target.value});
   };
 
   handleSelectButton (event) {
     let a = this;
+    var gameData = {};
+    for(var property in a.state) {
+      if(a.state.hasOwnProperty(property) && (property.substring(0,1) === 'w' || property.substring(0,1) === 'b')){
+        gameData[property] = a.state[property];
+      }
+    }
     this.state.databaseRef.once('value').then(function(snapshot) {
       var games = snapshot.val();
-      console.log(games);
       if(games === null) {
         var info = {
-          whiteTurn: a.state.whiteTurn,
-          user1: a.state.user1,
-          gameData: [],
-        }
-        a.state.databaseRef.push(info);
+          user1: a.state.username,
+          user2: '',
+          gameData: gameData,
+        };
+        var gameID = a.state.databaseRef.push(info).key
+        a.setState({
+          username: a.state.username,
+          isWhite: true,
+          gameID: gameID,
+          submitted: true,
+        });
+        var user2Joins = firebase.database().ref('/games/' + gameID);
+        user2Joins.on('child_changed', function(snapshot) {
+          user2Joins.off('child_changed');
+          var gameListener = firebase.database().ref('/games/' + gameID + '/gameData/');
+          gameListener.on('child_changed', function(snapshot) {
+            if(snapshot.key.substring(0,6) === 'white_' || snapshot.key.substring(0,6) === 'black_'){
+              console.log(snapshot.key);
+              console.log(snapshot.val());
+              var data = snapshot.val();
+              data['piece'] = snapshot.key;
+              movePiece(data);
+            }
+          });
+          a.setState({
+            otherUser: snapshot.val()
+          });
+        });
       } else {
         for(var property in games) {
           if(games.hasOwnProperty(property)) {
-            if(games.user1 === null) {
-              
+            if(games[property]['user2'] === '') {
+              var postData ={
+                user1: games[property]['user1'],
+                user2: a.state.username,
+                gameData: games[property]['gameData']
+              }
+              var updates = {};
+              updates['/games/' + property] = postData;
+              firebase.database().ref().update(updates);
+              var gameListener = firebase.database().ref('/games/' + property + '/gameData/');
+              gameListener.on('child_changed', function(snapshot) {
+                if(snapshot.key.substring(0,6) === 'white_' || snapshot.key.substring(0,6) === 'black_'){
+                  console.log(snapshot.key);
+                  console.log(snapshot.val());
+                  var data = snapshot.val();
+                  data['piece'] = snapshot.key;
+                  movePiece(data);
+                }
+              });
+              a.setState({
+                username: a.state.username,
+                otherUser: games[property]['user1'],
+                submitted: true,
+                gameID: property,
+              });
             }
           }
         }
@@ -153,12 +218,12 @@ class gamescene extends Component {
   render() {
     return (
       <div className="Gamescene">
-        <h2>{this.state.blackWin ? "Black won!" : this.state.whiteWin ? "White won!" : this.state.whiteTurn ? "White's Turn" : "Black's Turn"}</h2>
-        {this.state.submitted ? 
-          <h2> Username: {this.state.user1}</h2>
-           : 
-          <form> 
-            Username: <input type="text" name="FirstName" value={this.state.user1} onChange= {this.handleFormChange}/>
+        <h2>{this.state.blackWin ? "Black won!" : this.state.whiteWin ? "White won!" : this.state.whiteTurn ? (this.state.isWhite ? "Your Turn" : "Their Turn") : (this.state.isWhite ? "Their Turn" : "Your Turn")}</h2>
+        {this.state.submitted ?
+          <p> Username: {this.state.username}  Opponent: {this.state.otherUser}</p>
+           :
+          <form>
+            Username: <input type="text" name="FirstName" value={this.state.username} onChange= {this.handleFormChange}/>
             <button type="button" onClick={this.handleSelectButton}>Submit</button>
           </form>
         }

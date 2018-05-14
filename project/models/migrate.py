cbc9f084733432cb15ef4246a32b2ab3fb5592cf
sherwyn <sherwyn.sen@gmail.com>
2018-05-14 02:58:36 -0700

    more modular migrations

--- project/models/migrate.py
+++ project/models/migrate.py
@@ -4,8 +4,12 @@ migrate.py
 tools for updating items in the database
 """
 from project import pyre_db
+import os
 
-def add_user_property(property_name: str, default_value):
+USER_BRANCH = os.getenv("USER_BRANCH") or "users"
+ROOM_BRANCH = os.getenv("ROOM_BRANCH") or "SuicideChess"
+
+def add_branch_property(branch_name: str, property_name: str, default_value):
     '''
     add a new property for each user
     :param property_name:
@@ -13,9 +17,15 @@ def add_user_property(property_name: str, default_value):
     :return:
     '''
 
-    users = pyre_db.child("users").get().val()
-    if users:
-        for user_id, user_data in users.items():
-            user_data[property_name] = default_value
-            pyre_db.child("users").child(user_id).set(user_data)
+    children = pyre_db.child(branch_name).get().val()
+    if children:
+        for child_id, child_data in children.items():
+            child_data[property_name] = default_value
+            pyre_db.child(branch_name).child(child_id).set(child_data)
 
+
+def add_user_property(property_name: str, default_value):
+    add_branch_property(USER_BRANCH, property_name, default_value)
+
+def add_room_property(property_name: str, default_value):
+    add_branch_property(ROOM_BRANCH, property_name, default_value)

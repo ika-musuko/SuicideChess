ad83456827c22333ebd449df91af2f94bd2eefa5
Eric Scaramuzzo <eric.scaramuzzo@gmail.com>
2018-05-14 12:06:39 -0700

    Test fix to Issue # 13

--- suicide_chess_react/src/firebase.js
+++ suicide_chess_react/src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase'
 var config = {
     apiKey: "AIzaSyCXou2TZBE14UqO0ZuhOyCk8k-pBgSHkqQ",
     authDomain: "suicide-chess.firebaseapp.com",
-    databaseURL: "https://suicide-chess.firebaseio.com",
+    databaseURL: "https://suicide-chess.firebaseio.com/",
     projectId: "suicide-chess",
     storageBucket: "suicide-chess.appspot.com",
     messagingSenderId: "306643895755"

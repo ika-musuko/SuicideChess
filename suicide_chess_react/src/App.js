46caf200238af4a4bc90cc703b8511d7e9e5b907
Eric Scaramuzzo <eric.scaramuzzo@gmail.com>
2018-05-17 16:38:45 -0700

    Fix css override

--- suicide_chess_react/src/App.js
+++ suicide_chess_react/src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Suicide Chess</h1>
         <Gamescene roomID={this.props.roomID} user={this.props.user}/>
       </div>
     );

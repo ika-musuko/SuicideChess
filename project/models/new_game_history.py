6d1630ca1ef0b6e29bf1739a5427445757e8fb56
sherwyn <sherwyn.sen@gmail.com>
2018-05-18 13:30:18 -0700

    added losers

--- project/models/new_game_history.py
+++ project/models/new_game_history.py
@@ -1,10 +1,11 @@
 import datetime
 
 
-def new_game_history(move_list: list, winner: str, last_board: dict) -> dict:
+def new_game_history(move_list: list, winner: str, loser: str, last_board: dict) -> dict:
     return {
         "timeFinished": str(datetime.datetime.today())  # string of the time finished
         , "moveList": move_list  # list of the moves
         , "winner": winner
+        , "loser": loser
         , "lastBoard" : last_board
     }
